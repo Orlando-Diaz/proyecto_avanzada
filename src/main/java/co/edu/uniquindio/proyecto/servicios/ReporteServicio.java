15032bd73b066dbe6c170d2ef3f954aedf5a3b93
Orlando <orlando.diazr@uqvirtual.edu.co>
2025-03-28 20:56:19 -0500

    actualización 2

--- src/main/java/co/edu/uniquindio/proyecto/servicios/ReporteServicio.java
+++ src/main/java/co/edu/uniquindio/proyecto/servicios/ReporteServicio.java
@@ -1,6 +1,32 @@
 package co.edu.uniquindio.proyecto.servicios;
 
+import co.edu.uniquindio.proyecto.dto.CrearUsuarioDTO;
+import co.edu.uniquindio.proyecto.dto.EditarUsuarioDTO;
+import co.edu.uniquindio.proyecto.dto.ReporteDTO;
+import co.edu.uniquindio.proyecto.dto.UsuarioDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public interface ReporteServicio {
+
+    public static void crearReporte(ReporteDTO reporte) throws Exception{
+    }
+
+    public static void editarReporte(ReporteDTO Reporte) throws Exception{
+    }
+
+    public static void eliminarReporte(String id) throws Exception{
+    }
+
+    public static ReporteDTO obtenerReportes(String id) throws Exception{
+        return null;
+    }
+
+    public static List<ReporteDTO> listarTodos(){
+        return List.of();
+    }
+
+    List<ReporteDTO> listarTodos(String nombre, String ciudad);
 }
