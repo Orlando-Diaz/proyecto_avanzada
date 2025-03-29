3d34192d4440e88c5a5fc4fae70c490f60df206b
SalomePerez <salome.perezf@uqvirtual.edu.co>
2025-03-28 21:28:59 -0500

    actualizaciones

--- src/main/java/co/edu/uniquindio/proyecto/servicios/Impl/UsuarioServicioImpl.java
+++ src/main/java/co/edu/uniquindio/proyecto/servicios/Impl/UsuarioServicioImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
     @Service
-    class usuarioServicioImpl implements usuarioServicio {
+    class UsuarioServicioImpl implements usuarioServicio {
 
         @Override
         public List<UsuarioDTO> listarTodos(String nombre, String ciudad) {
