a641a6b71f179380d4424fd5f76943a628468897
SalomePerez <salome.perezf@uqvirtual.edu.co>
2025-04-08 18:16:28 -0500

    Modificacion en CrearUsuarioDTO. Se cambia ciudad de tipo String y se coloco como Ciudad (enum)

--- src/main/java/co/edu/uniquindio/proyecto/dto/CrearUsuarioDTO.java
+++ src/main/java/co/edu/uniquindio/proyecto/dto/CrearUsuarioDTO.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.proyecto.dto;
 
+import co.edu.uniquindio.proyecto.modelo.enums.Ciudad;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
@@ -7,7 +8,7 @@ import org.hibernate.validator.constraints.Length;
 public record CrearUsuarioDTO(
         @NotBlank @Length(max = 100) String nombre,
         @Length(max = 10) String telefono,
-        @NotBlank @Length(max = 100) String ciudad,
+        Ciudad ciudad,
         @NotBlank @Length(max = 100) String direccion,
         @NotBlank @Length(max = 50) @Email String email,
         @NotBlank @Length(min = 7, max = 20) String password
